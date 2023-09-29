a9103ccb4642df7821584b9b5c70f9905b5ca110
João Fonseca <joaofvfonseca@gmail.com>
2023-09-29 17:24:08 +0100

    Add tracing and cli argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(clap::Parser)]
+#[command(author, version, about, long_about = None)]
+struct ClapParser {
+    /// Overwrites the default config file and creates a fresh one
+    #[arg(long)]
+    reset: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    configure_tracing();
+    tracing::info!("CSCTRL Version {}", env!("CARGO_PKG_VERSION"));
+}
+
+fn configure_tracing() {
+    tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(tracing::level_filters::LevelFilter::TRACE)
+            .with_target(false)
+            .finish())
+        .expect("Failed tracing subscriber creation");
 }
