a7e34a17668e628345b56ceb8c48e4ce80619856
João Fonseca <joaofvfonseca@gmail.com>
2023-10-03 17:36:01 +0100

    Initial layout for the TUI

--- src/terminal/terminal.rs
+++ src/terminal/terminal.rs
@@ -6,24 +6,37 @@ use crate::csctrl::types::CsServerConfig;
 use std::cell::OnceCell;
 use std::ops::Deref;
 use std::time::Duration;
-use crossterm::event::{Event, KeyCode};
+use crossterm::event::{Event, KeyCode, KeyEventKind};
 use ratatui::Frame;
+use ratatui::layout::{Constraint, Layout};
+use ratatui::prelude::{Alignment, Direction};
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::text::Text;
+use ratatui::widgets::{Block, Borders, Paragraph};
+
+struct TerminalUiState {
+    selected_server: CsServerConfig,
+    input_box: String,
+}
 
 pub struct Terminal {
-    selected_server: CsServerConfig,
     terminal_ui: OnceCell<ratatui::Terminal<CrosstermBackend<Stdout>>>,
-    is_terminal_active: bool
+    is_terminal_active: bool,
+    terminal_ui_state: TerminalUiState,
 }
 
 impl Terminal {
     pub fn terminal() -> Terminal {
         Terminal {
-            selected_server: CsServerConfig {
-                name: "".to_string(),
-                address: "".to_string(),
-                rcon_address: "".to_string(),
-                rcon_password: "".to_string(),
-                csctrl_token: "".to_string(),
+            terminal_ui_state: TerminalUiState {
+                selected_server: CsServerConfig {
+                    name: "".to_string(),
+                    address: "".to_string(),
+                    rcon_address: "".to_string(),
+                    rcon_password: "".to_string(),
+                    csctrl_token: "".to_string(),
+                },
+                input_box: "".to_string(),
             },
             terminal_ui: OnceCell::new(),
             is_terminal_active: false,
@@ -44,14 +57,28 @@ impl Terminal {
     pub fn tick(&mut self) {
         self.handle_events();
 
-        self.terminal_ui.get_mut().unwrap().draw(ui).unwrap();
+        self.terminal_ui.get_mut().unwrap().draw(|frame| {
+            ui(&mut self.terminal_ui_state, frame);
+        }).unwrap();
     }
 
     fn handle_events(&mut self) {
         if !crossterm::event::poll(Duration::from_millis(50)).unwrap() { return; }
 
         if let Event::Key(key) = crossterm::event::read().unwrap() {
-            if KeyCode::Char('q') == key.code { self.close_terminal(); }
+            match key.code {
+                KeyCode::Char(value) => {
+                    if !validate_input_char(&value) { return; }
+                    if key.kind != KeyEventKind::Press { return; }
+                    &self.terminal_ui_state.input_box.push(value);
+                }
+                KeyCode::Backspace => {
+                    if key.kind != KeyEventKind::Press { return; }
+                    &self.terminal_ui_state.input_box.pop();
+                }
+                KeyCode::Esc => { self.close_terminal(); }
+                _ => {}
+            }
         }
     }
 
@@ -71,6 +98,29 @@ impl Terminal {
     }
 }
 
-fn ui(frame: &mut Frame<CrosstermBackend<Stdout>>) {
+fn validate_input_char(char: &char) -> bool {
+    let char_code = *char as u32;
+    if char_code > 127 { return false; }
+    return true;
+}
 
+fn ui(state: &mut TerminalUiState, frame: &mut Frame<CrosstermBackend<Stdout>>) {
+    let terminal_height = frame.size().height;
+
+    let layout_main = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Length(terminal_height - 1),
+            Constraint::Length(1),
+        ])
+        .split(frame.size());
+
+    frame.render_widget(Block::new().title("CSCTRL".red().bold().underlined()).borders(Borders::all()), layout_main[0]);
+
+    let time_in_secs = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+    let cursor = if time_in_secs % 2 == 0 { "█" } else { "" };
+    frame.render_widget(
+        Paragraph::new(format!("> {}{}", state.input_box, cursor)),
+        layout_main[1]
+    );
 }
